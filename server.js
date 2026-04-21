6e2267301803c7333cf9b5c507499905dc148280
satya vardhan <satyavardhand19@gmail.com>
2026-04-21 11:35:08 +0530

    fixed the error

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 const app = express();
 app.use(cors());
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, "public")));
 
 const SESSION_ID = process.env.SESSION_ID || "";
 
